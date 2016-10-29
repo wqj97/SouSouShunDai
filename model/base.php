b3ce7cb49cadbf47a822d6da4e9c48bb510d3d30
wqj97 <wqj97@126.com>
2016-10-29 11:05:23 +0800

    update WX constants

--- model/base.php
+++ model/base.php
@@ -8,10 +8,10 @@
 //ini_set("display_errors","off");
 $_host = "127.0.0.1";
 $_sqlUser = "root";
-$_sqlPwd = "Wanqj97...";
+$_sqlPwd = "wqj9705";
 $_sqlDB = "Express";
 $sql = new mysqli($_host,$_sqlUser,$_sqlPwd,$_sqlDB);
 $sql->query("set names utf8");
-define("appId","");
-define("AppSecret","");
+define("appId","wx573174f0dbdb80b2");
+define("AppSecret","986759a218f79dbe22683dfedc94dfa2");
 define("redirectURL","http%3a%2f%2fdq.97qingnian.com%2fapi%2fgetAccess.php");
