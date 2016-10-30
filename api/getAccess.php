ab7ef913dfcd4509292180f09dda28c616eff801
wqj97 <wqj97@126.com>
2016-10-30 19:08:38 +0800

    update userAPIs

--- api/getAccess.php
+++ api/getAccess.php
@@ -15,20 +15,20 @@ if (isset($_GET["code"])) {
     if (isset($access["errcode"])) {
         header("location:../webAccess.php");
     }
-    $userInfo = $sql->query("select Id,openid,refresh from `user` where `openid` = '$access[openid]'")->fetch_array();
+    $userInfo = $sql->query("select Id,openId,RefreshToken from `user` where `openId` = '$access[openid]'")->fetch_array();
     if ($userInfo) {
-        $expire = $sql->query("select ACT_expires from `user` where Id = $userInfo[Id]")->fetch_row()[0];
+        $expire = $sql->query("select ACTexpires from `user` where Id = $userInfo[Id]")->fetch_row()[0];
         if ($expire <= time()) {
             $url = "https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=" . appId . "&grant_type=refresh_token&refresh_token=" . $userInfo[2];
             $access = json_encode(file_get_contents($url), true);
             $time = time() + $access["expires_in"];
-            $sql->query("update `user` set `sessionId` = '$access[access_token]',ACT_expires = '$time',`refresh`='$access[refresh_token]' WHERE Id = '$userInfo[0]'");
+            $sql->query("update `user` set `AccessToken` = '$access[access_token]',ACTexpires = '$time',`RefreshToken`='$access[refresh_token]' WHERE Id = '$userInfo[0]'");
         }
     } else {
         $url = "https://api.weixin.qq.com/sns/userinfo?access_token=$access[access_token]&openid=$access[openid]&lang=zh_CN";
         $userInfo = json_decode(file_get_contents($url), true);
         $time = time() + $access["expires_in"];
-        $sql->query("insert into `user` (`name`,`openid`,`seesionId`,`ACT_expires`,`refresh`,`head`) VALUE ('$userInfo[nickname]','$userInfo[openid]','$access[access_token]','$time','$access[refresh_token]','$userInfo[headimgurl]')");
+        $sql->query("insert into `user` (`name`,`openId`,`AccessToken`,`ACTexpires`,`RefreshToken`,`head`) VALUE ('$userInfo[nickname]','$userInfo[openid]','$access[access_token]','$time','$access[refresh_token]','$userInfo[headimgurl]')");
         $userInfo["Id"] = $sql->query("select max(Id) from `user`")->fetch_row()[0];
 //        echo $sql->error;
     }
