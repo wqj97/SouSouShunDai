a6ebd1e0734bf7c1fed0098944ce3a2bcb4bd69c
wqj97 <wqj97@126.com>
2016-10-30 20:58:38 +0800

    bug fixed about user.php

--- api/user.php
+++ api/user.php
@@ -10,12 +10,12 @@ require_once '../model/base.php';
 global $sql;
 $type = $sql->query("select `type` from `user` where Id = $_SESSION[UID]");
 $type = $type->fetch_row();
-if ($type[0] != 1) exit('<h1>非法操作!</h1>');
+if ($type[0] != 0) exit('<h1>非法操作!</h1>');
 require_once '../model/user.php';
 
 $u = new UserUtils();
 
-switch ($_POST['action']){
+switch ($_GET['action']){
 	case 'newUser':
 		$r = $u->newUser($_POST['name'],$_POST['sex']);
 		echo json_encode($r,JSON_UNESCAPED_UNICODE);
