10c8c8e8fa5506d033f79e34b860960de965944e
Allen97 <w894224650@gmail.com>
2016-10-30 21:15:49 +0800

    Update order.php

--- api/order.php
+++ api/order.php
@@ -3,13 +3,14 @@
  * Created by PhpStorm.
  * User: wangchun
  * Date: 2016/10/30
+ * Time: 10:37
  */
 session_start();
 require_once "../model/base.php";
 global $sql;
 $type = $sql->query("select `type` from `user` where Id = $_SESSION[UID]");
 $type = $type->fetch_row();
-if ($type[0] != 1) {
+if ($type[0] != 0) {
     exit("<h1 style='color: red'>非法操作!</h1>");
 }
 require_once "../model/order.php";
@@ -17,19 +18,19 @@ require_once "../model/order.php";
 $order = new order();
 switch ($_GET['action']){
     case "new":
-            echo json_encode($order->newOrder($_POST["date"],$_POST["price"],$_POST["expire"],$_POST["size"],$_POST["remark"],$_POST["receiveTime"],JSON_UNESCAPED_UNICODE));
+            echo $order->newOrder($_POST["date"],$_POST["price"],$_POST["expire"],$_POST["size"],$_POST["remark"],$_POST["receiveTime"]);
         break;
     case "getAll":
-            echo json_encode($order->getOrder($_POST["page"]),JSON_UNESCAPED_UNICODE);
+            echo $order->getOrder($_POST["page"]);
         break;
     case "get":
-            echo json_encode($order->getOrderById($_POST["Id"]),JSON_UNESCAPED_UNICODE);
+            echo $order->getOrderById($_POST["Id"]);
         break;
     case "edit":
-            echo json_encode($order->upOrder($_POST["Id"],$_POST["date"],$_POST["price"],$_POST["expire"],$_POST["size"],$_POST["remark"],$_POST["receiveTime"],JSON_UNESCAPED_UNICODE));
+            echo $order->upOrder($_POST["date"],$_POST["price"],$_POST["expire"],$_POST["size"],$_POST["remark"],$_POST["receiveTime"]);
         break;
     case "phone":
-            echo json_encode($order->getPhone($_POST["Id"],JSON_UNESCAPED_UNICODE));
+            echo $order->getPhone($_POST["Id"]);
         break;
 
 
