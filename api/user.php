257d3354c65002ed9806b8c97a3eb65b8d30a4c1
wqj97 <wqj97@126.com>
2016-10-30 22:41:52 +0800

    release user API

--- api/user.php
+++ api/user.php
@@ -16,27 +16,28 @@ require_once '../model/user.php';
 $u = new UserUtils();
 
 switch ($_GET['action']){
-	case 'newUser':
-		$r = $u->newUser($_POST['name'],$_POST['sex']);
-		echo json_encode($r,JSON_UNESCAPED_UNICODE);
-		exit;
+//	case 'newUser':
+//		$r = $u->newUser($_POST['name'],$_POST['sex']);
+//		echo json_encode($r,JSON_UNESCAPED_UNICODE);
+//		exit;
 	case 'edit':
-		$u->updateNameSex($_POST['Id'],$_POST['name'],$_POST['sexual'],$_POST['position']);
-		exit;
-	case 'editPhone':
+		$u->updateNameSexPos($_POST['Id'],$_POST['name'],$_POST['sexual'],$_POST['position']);
+        echo json_encode(["result"=>"成功"],256);
+        break;
+	case 'phone':
 		$r = $u->updateTel($_POST['Id'],$_POST['phone']);
-		echo json_encode($r,JSON_UNESCAPED_UNICODE);
-		exit;
+        echo json_encode($r,256);
+        break;
 	case 'getUser':
 		$r = $u->getUserInfo($_POST['Id']);
 		echo json_encode($r,JSON_UNESCAPED_UNICODE);
-		exit;
-	case 'getPublisherByOrder':
-		$r = $u->getPublisherByOrder($_POST['orderId']);
-		echo json_encode($r,JSON_UNESCAPED_UNICODE);
-		exit;
-	case 'getCourierByOrder':
-		$r = $u->getCourierByOrder($_POST['orderId']);
-		echo json_encode($r,JSON_UNESCAPED_UNICODE);
-		exit;
+        break;
+//	case 'getPublisherByOrder':
+//		$r = $u->getPublisherByOrder($_POST['orderId']);
+//		echo json_encode($r,JSON_UNESCAPED_UNICODE);
+//        break;
+//	case 'getCourierByOrder':
+//		$r = $u->getCourierByOrder($_POST['orderId']);
+//		echo json_encode($r,JSON_UNESCAPED_UNICODE);
+//        break;
 }
